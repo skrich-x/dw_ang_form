e5e120ad2ab2c54e54a5bde63a882b4cd06b0cf8
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-14 21:28:09 -0400

    initial yeoman angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dwAngFormApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dwAngFormApp
+ */
+angular.module('dwAngFormApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dwAngFormApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dwAngFormApp
+ */
+angular.module('dwAngFormApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
